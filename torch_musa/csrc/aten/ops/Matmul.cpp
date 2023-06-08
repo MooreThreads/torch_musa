6bb7e6021ca61ca3358f2e22ec17048ddf11edda
WangZaixing <zaixing.wang@mthreads.com>
2023-06-08 12:24:44 +0800

    feat(op):improve addmm && refactor code (#161)
    * improve mm
    
    * update
    
    * clean
    
    * lint
    
    * update
    
    Co-authored-by: wzx <1>

--- torch_musa/csrc/aten/ops/Matmul.cpp
+++ torch_musa/csrc/aten/ops/Matmul.cpp
@@ -25,7 +25,15 @@ bool IsTranspose(const Tensor& mat, bool is_batch) {
   }
 }
 
-void BmmCall(const Tensor& l, const Tensor& r, Tensor& out, bool is_batch) {
+void MmCall(
+    const Tensor& l,
+    const Tensor& r,
+    const Tensor& bias,
+    Tensor& out,
+    bool is_batch = false,
+    const at::Scalar& alpha = 1,
+    const at::Scalar beta = 0,
+    const at::Scalar gama = 0) {
   c10::musa::MUSAGuard device_guard(l.device());
   muHandle& h = GetMudnnHandle();
   bool trans_l = IsTranspose(l, is_batch);
@@ -42,78 +50,29 @@ void BmmCall(const Tensor& l, const Tensor& r, Tensor& out, bool is_batch) {
       : CreateMUTensor(Contiguous(r, contiguous_r));
   auto rst = CreateMUTensor(out);
   ConfigFormat(out, rst, true);
-  ::musa::dnn::BatchMatMul b_mm;
-  CHECK_MUDNN_STATUS(b_mm.SetTranspose(trans_l, trans_r), "SetTranspose");
-  CHECK_MUDNN_STATUS(b_mm.Run(h, rst, lmt, rmt), "Run");
-}
-
-void MmCall(
-    const Tensor& l,
-    const Tensor& r,
-    Tensor& out,
-    double alpha,
-    double beta) {
-  c10::musa::MUSAGuard device_guard(l.device());
-  muHandle& h = GetMudnnHandle();
-  bool trans_l = IsTranspose(l, false);
-  bool trans_r = IsTranspose(r, false);
-  Tensor contiguous_l;
-  Tensor contiguous_r;
-  auto lmt = trans_l ? CreateMUTensor(l.t(), true)
-                     : CreateMUTensor(Contiguous(l, contiguous_l));
-  auto rmt = trans_r ? CreateMUTensor(r.t(), true)
-                     : CreateMUTensor(Contiguous(r, contiguous_r));
-  auto rst = CreateMUTensor(out);
-  ConfigFormat(out, rst, true);
-  ::musa::dnn::MatMul matmul;
-  CHECK_MUDNN_STATUS(matmul.SetTranspose(trans_l, trans_r), "SetTranspose");
-  CHECK_MUDNN_STATUS(matmul.SetAlpha(alpha), "SetAlpha");
-  CHECK_MUDNN_STATUS(matmul.SetBeta(beta), "SetBeta");
-  CHECK_MUDNN_STATUS(matmul.Run(h, rst, lmt, rmt), "Run");
-}
-
-Tensor& MmAlphaBetaOut(
-    const Tensor& l,
-    const Tensor& r,
-    const double alpha,
-    const double beta,
-    Tensor& out) {
-  c10::musa::MUSAGuard device_guard(l.device());
-  muHandle& h = GetMudnnHandle();
-  auto dim_tensor1 = l.dim();
-  auto dim_tensor2 = r.dim();
-
-  if (dim_tensor1 == 4 || dim_tensor2 == 4) {
-    auto view_l = l;
-    auto view_r = r;
-    if (dim_tensor1 == 4) {
-      view_l = view_l.view({-1, view_l.size(2), view_l.size(3)});
-    }
-    if (dim_tensor2 == 4) {
-      view_r = view_r.view({-1, view_r.size(2), view_r.size(3)});
-    }
-    auto view_out = out.view({-1, out.size(2), out.size(3)});
-    BmmCall(view_l, view_r, view_out, false);
-  } else if (dim_tensor1 == 3 || dim_tensor2 == 3) {
-    // batch-mm
-    BmmCall(l, r, out, true);
+  if (is_batch) {
+    ::musa::dnn::BatchMatMul b_mm;
+    CHECK_MUDNN_STATUS(b_mm.SetTranspose(trans_l, trans_r), "SetTranspose");
+    CHECK_MUDNN_STATUS(b_mm.Run(h, rst, lmt, rmt, InternalMemAlloc), "Run");
   } else {
-    // when dim_tensor1 <= 2 &&  dim_tensor1 <= 2 ,call dot
-    if (alpha == 1 && beta == 0) {
-      ::musa::dnn::Dot dot;
-      auto contiguous_out = Contiguous(out);
-      auto rst = CreateMUTensor(contiguous_out);
-      ConfigFormat(out, rst, true);
-      auto contiguous_l = Contiguous(l);
-      auto contiguous_r = Contiguous(r);
-      auto lmt = CreateMUTensor(contiguous_l);
-      auto rmt = CreateMUTensor(contiguous_r);
-      CHECK_MUDNN_STATUS(dot.Run(h, rst, lmt, rmt), "Run");
+    ::musa::dnn::MatMul mm;
+    CHECK_MUDNN_STATUS(mm.SetTranspose(trans_l, trans_r), "SetTranspose");
+    // not support broadcast
+    if (alpha.equal(1) && beta.equal(0) && gama.equal(1) &&
+        bias.numel() == out.size(1)) {
+      auto contiguous_bias = Contiguous(bias);
+      auto bmt = CreateMUTensor(contiguous_bias);
+      // will support set_gamma in the future
+      // CHECK_MUDNN_STATUS(mm.SetGamma(gama.to<double>()), "SetGamma");
+      CHECK_MUDNN_STATUS(
+          mm.RunWithBiasAdd(h, rst, lmt, rmt, bmt, InternalMemAlloc),
+          "RunWithBiasAdd");
     } else {
-      MmCall(l, r, out, alpha, beta);
+      CHECK_MUDNN_STATUS(mm.SetAlpha(alpha.to<double>()), "SetAlpha");
+      CHECK_MUDNN_STATUS(mm.SetBeta(beta.to<double>()), "SetBeta");
+      CHECK_MUDNN_STATUS(mm.Run(h, rst, lmt, rmt), "Run");
     }
   }
-  return out;
 }
 
 at::Tensor& AddMmOut(
@@ -124,47 +83,17 @@ at::Tensor& AddMmOut(
     const at::Scalar& alpha,
     at::Tensor& out) {
   TORCH_CHECK(
-      mat1.dim() == 2, "mat1 must be a matrix, got ", mat1.dim(), "-D tensor");
-  TORCH_CHECK(
-      mat2.dim() == 2, "mat2 must be a matrix, got ", mat2.dim(), "-D tensor");
-  TORCH_CHECK(
-      mat1.sizes()[1] == mat2.sizes()[0],
-      "mat1 and mat2 shapes cannot be multiplied (",
-      mat1.sizes()[0],
-      "x",
-      mat1.sizes()[1],
-      " and ",
-      mat2.sizes()[0],
-      "x",
-      mat2.sizes()[1],
-      ")");
-  TORCH_CHECK(
-      self.scalar_type() == at::ScalarType::Float,
-      "Dtype of input tensor of Addmm only support Float32, but now it is ",
-      self.scalar_type());
-  TORCH_CHECK(
-      mat1.scalar_type() == at::ScalarType::Float,
-      "Dtype of mat1 tensor of Addmm only support Float32, but now it is ",
-      mat1.scalar_type());
-  TORCH_CHECK(
-      mat2.scalar_type() == at::ScalarType::Float,
-      "Dtype of mat2 tensor of Addmm only support Float32, but now it is ",
-      mat2.scalar_type());
-
-  c10::musa::MUSAGuard device_guard(self.device());
-  out.zero_();
-  out.add_(self);
-  // only support float32 now
-  AT_DISPATCH_ALL_MTGPU_TYPES_AND_HALF(self.scalar_type(), "add_mm", [&] {
-    auto beta_value = beta.to<scalar_t>();
-    auto alpha_value = alpha.to<scalar_t>();
-    MmAlphaBetaOut(
-        mat1,
-        mat2,
-        static_cast<double>(alpha_value),
-        static_cast<double>(beta_value),
-        out);
-  });
+      mat1.dim() == 2 && mat2.dim() == 2 && mat1.size(1) == mat2.size(0),
+      "mat1 and mat2 must be a matrix and mat1_shape[1] must equal to "
+      "mat2_shape[0]");
+  // normal case
+  if (alpha.equal(1) && beta.equal(1) && self.numel() == out.size(1)) {
+    MmCall(mat1, mat2, self, out, false, 1, 0, 1);
+  } else {
+    out.zero_();
+    out.add_(self);
+    MmCall(mat1, mat2, out, out, false, alpha, beta, 0);
+  }
   return out;
 }
 
@@ -185,10 +114,16 @@ at::Tensor AddMm(
   return result;
 }
 
-Tensor Mm(const Tensor& self, const Tensor& mat2) {
+Tensor& MmOut(const Tensor& self, const Tensor& mat2, Tensor& out) {
   TORCH_CHECK(
       self.dim() == 2 && mat2.dim() == 2 && self.size(1) == mat2.size(0),
-      "self must be a matrix and self_shape[1] must equal to mat2_shape[0]");
+      "self and mat2 must be a matrix and self_shape[1] must equal to "
+      "mat2_shape[0]");
+  MmCall(self, mat2, out, out, false, 1, 0);
+  return out;
+}
+
+Tensor Mm(const Tensor& self, const Tensor& mat2) {
   Tensor result = empty_mtgpu(
       {self.size(0), mat2.size(1)},
       self.scalar_type(),
@@ -196,33 +131,7 @@ Tensor Mm(const Tensor& self, const Tensor& mat2) {
       self.device(),
       c10::nullopt,
       at::MemoryFormat::Contiguous);
-  BmmCall(self, mat2, result, false);
-  return result;
-}
-
-Tensor& MmOut(const Tensor& self, const Tensor& mat2, Tensor& out) {
-  TORCH_CHECK(
-      self.dim() == 2 && mat2.dim() == 2 && self.size(1) == mat2.size(0),
-      "self and mat2 must be a matrix and self_shape[1] must equal to "
-      "mat2_shape[0]");
-  MmCall(self, mat2, out, 1, 1);
-  return out;
-}
-
-Tensor Bmm(const Tensor& self, const Tensor& mat2) {
-  TORCH_CHECK(self.dim() == 3 && mat2.dim() == 3, "self must be a 3D matrix");
-  TORCH_CHECK(
-      self.size(0) == mat2.size(0) && self.size(2) == mat2.size(1),
-      "self_shape[0] must equal to mat2_shape[0], and self_shape[2] "
-      "must equal to mat2_shape[1]");
-  Tensor result = empty_mtgpu(
-      {self.size(0), self.size(1), mat2.size(2)},
-      self.scalar_type(),
-      c10::nullopt,
-      self.device(),
-      c10::nullopt,
-      at::MemoryFormat::Contiguous);
-  BmmCall(self, mat2, result, true);
+  MmOut(self, mat2, result);
   return result;
 }
 
@@ -232,8 +141,20 @@ Tensor& BmmOut(const Tensor& self, const Tensor& mat2, Tensor& out) {
       self.size(0) == mat2.size(0) && self.size(2) == mat2.size(1),
       "self_shape[0] must equal to mat2_shape[0], and self_shape[2] "
       "must equal to mat2_shape[1]");
-  BmmCall(self, mat2, out, true);
+  MmCall(self, mat2, out, out, true);
   return out;
+}
+
+Tensor Bmm(const Tensor& self, const Tensor& mat2) {
+  Tensor result = empty_mtgpu(
+      {self.size(0), self.size(1), mat2.size(2)},
+      self.scalar_type(),
+      c10::nullopt,
+      self.device(),
+      c10::nullopt,
+      at::MemoryFormat::Contiguous);
+  BmmOut(self, mat2, result);
+  return result;
 }
 
 TORCH_LIBRARY_IMPL(aten, PrivateUse1, m) {
