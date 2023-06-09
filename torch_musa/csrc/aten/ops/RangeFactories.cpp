6808b2cbb0bb7ef063b38f3e62347f4e0a32bc61
KeGeyang <geyang.ke@mthreads.com>
2023-06-09 18:43:12 +0800

    Rife (#165)
    * test rife
    
    * c lint
    
    * py lint
    
    * add test for backward
    
    * bwd op test failed
    
    * fwd test passed

--- torch_musa/csrc/aten/ops/RangeFactories.cpp
+++ torch_musa/csrc/aten/ops/RangeFactories.cpp
@@ -2,8 +2,11 @@
 #include <ATen/Config.h>
 #include <ATen/NativeFunctions.h>
 #include <ATen/Parallel.h>
+#include <ATen/core/op_registration/adaption.h>
+
 #include <torch/library.h>
 
+#include "torch_musa/csrc/aten/ops/TensorFactory.h"
 #include "torch_musa/csrc/aten/utils/Utils.h"
 #include "torch_musa/csrc/core/MUSAGuard.h"
 
@@ -44,9 +47,22 @@ Tensor& ArangeOut(const Scalar& end, Tensor& result) {
   return ArangeStartOut(/*start=*/0, end, /*step*/ 1, result);
 }
 
+at::Tensor& LinspaceOut(
+    const at::Scalar& start,
+    const at::Scalar& end,
+    int64_t steps,
+    at::Tensor& out) {
+  c10::optional<Device> common_device = nullopt;
+  c10::impl::check_and_update_common_device(
+      common_device, out, "LinspaceOut", "out");
+  const OptionalDeviceGuard device_guard(device_of(out));
+  return at::native::linspace_cuda_out(start, end, steps, out);
+}
+
 TORCH_LIBRARY_IMPL(aten, PrivateUse1, m) {
   m.impl("arange.out", &ArangeOut);
   m.impl("arange.start_out", &ArangeStartOut);
+  m.impl("linspace.out", &LinspaceOut);
 }
 
 } // namespace musa
