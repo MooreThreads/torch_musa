24a3ab686854af1f1cb0256fb1b6fb1d8a988d58
SongZehan <zehan.song@mthreads.com>
2023-07-26 13:11:56 +0800

    remove kCUDA -> kMUSA (#321)

--- tools/cuda_porting/cuda_porting.py
+++ tools/cuda_porting/cuda_porting.py
@@ -101,7 +101,6 @@ def port_cuda(
         "C10_CUDA_CHECK[(]": "TORCH_MUSA_CHECK(",
         "C10_CUDA_CHECK_WARN[(]": "TORCH_MUSA_WARN(",
         "C10_CUDA_API ": "",
-        "kCUDA": "kMUSA",
         "<ATen/CUDAFunctions\.h>" : "<ATen/MUSA_PORT_Functions.h>",
         "<c10/musa/CUDADeviceAssertionHost\.h>": "\"torch_musa/csrc/core/MUSADeviceAssertionHost.h\"",
         "<c10/musa/CUDAFunctions\.h>": "\"torch_musa/csrc/core/MUSAFunctions.h\"",
