82d39e42503425c0fb1a45e08a63b7a986335927
LiangZhenlin <zhenlin.liang@mthreads.com>
2023-08-22 14:08:45 +0800

    Feature/lzl/replication pad1d (#381)
    * support replication_pad1d.out

---
+++ torch_musa/csrc/aten/ops/ReplicationPad.cpp
@@ -0,0 +1,93 @@
+#include <ATen/Config.h>
+#include <ATen/NamedTensorUtils.h>
+#include <ATen/NativeFunctions.h>
+#include <ATen/core/op_registration/adaption.h>
+#include <ATen/native/Pool.h>
+#include <torch/library.h>
+
+#include "torch_musa/csrc/aten/ops/TensorFactory.h"
+#include "torch_musa/csrc/aten/utils/Utils.h"
+
+#include <mudnn.h>
+
+namespace at {
+namespace musa {
+
+struct structured_replication_pad1d_out_cuda_out final
+    : public at::native::structured_replication_pad1d_out_cuda {
+  structured_replication_pad1d_out_cuda_out(Tensor& out0)
+      : outputs_{std::ref(out0)} {}
+  void set_output_strided(
+      int64_t output_idx,
+      IntArrayRef sizes,
+      IntArrayRef strides,
+      TensorOptions options,
+      DimnameList names) override {
+    auto current_device = guard_.current_device();
+    if (C10_UNLIKELY(current_device.has_value())) {
+      TORCH_INTERNAL_ASSERT(
+          *current_device == options.device(),
+          "structured kernels don't support multi-device outputs");
+    } else {
+      guard_.reset_device(options.device());
+    }
+    const auto& out = outputs_[output_idx].get();
+    resize_out(out, sizes, strides, options);
+    // super must happen after, so that downstream can use maybe_get_output
+    // to retrieve the output
+  }
+  void set_output_raw_strided(
+      int64_t output_idx,
+      IntArrayRef sizes,
+      IntArrayRef strides,
+      TensorOptions options,
+      DimnameList names) override {
+    auto current_device = guard_.current_device();
+    if (C10_UNLIKELY(current_device.has_value())) {
+      TORCH_INTERNAL_ASSERT(
+          *current_device == options.device(),
+          "structured kernels don't support multi-device outputs");
+    } else {
+      guard_.reset_device(options.device());
+    }
+    const auto& out = outputs_[output_idx].get();
+    resize_out(out, sizes, strides, options);
+    if (!names.empty()) {
+      namedinference::propagate_names(outputs_[output_idx], names);
+    }
+    // super must happen after, so that downstream can use maybe_get_output
+    // to retrieve the output
+  }
+  const Tensor& maybe_get_output(int64_t output_idx) override {
+    return proxy_outputs_[output_idx].has_value() ? **proxy_outputs_[output_idx]
+                                                  : outputs_[output_idx].get();
+  }
+  std::array<std::reference_wrapper<Tensor>, 1> outputs_;
+  std::array<c10::optional<c10::ExclusivelyOwned<Tensor>>, 1> proxy_outputs_;
+  c10::musa::OptionalMUSAGuard guard_;
+};
+
+at::Tensor& ReplicationPad1dOut(
+    const at::Tensor& self,
+    at::IntArrayRef padding,
+    at::Tensor& out) {
+  c10::optional<Device> common_device = nullopt;
+  (void)common_device; // Suppress unused variable warning
+  c10::impl::check_and_update_common_device(
+      common_device, out, "ReplicationPad1dOut", "out");
+  c10::impl::check_and_update_common_device(
+      common_device, self, "ReplicationPad1dOut", "self");
+  structured_replication_pad1d_out_cuda_out op(out);
+  op.meta(self, padding);
+  op.impl(self, padding, op.maybe_get_output(0));
+  if (op.proxy_outputs_[0].has_value())
+    op.outputs_[0].get().copy_(**op.proxy_outputs_[0]);
+  return out;
+}
+
+TORCH_LIBRARY_IMPL(aten, PrivateUse1, m) {
+  m.impl("replication_pad1d.out", &ReplicationPad1dOut);
+}
+
+} // namespace musa
+} // namespace at
