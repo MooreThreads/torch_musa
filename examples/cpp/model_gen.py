23679a3267aa79ff74434c4b6a8e16196e98f787
ShangXiaokang <xiaokang.shang@mthreads.com>
2023-10-11 14:22:35 +0800

    fix(jit): support shallow copy for save/load between cpu and privateuse1 (#501)

--- examples/cpp/model_gen.py
+++ examples/cpp/model_gen.py
@@ -6,10 +6,10 @@ import torch_musa
 import torchvision
 
 model = torchvision.models.resnet50(pretrained=True)
-mdoel = model.eval().to("musa")
+model = model.eval().to("musa")
 
 input = torch.rand(1, 3, 224, 224).to("musa")
 model = torch.jit.trace(model, input, check_trace=False)
 
 model.save("resnet50.pt")
-print("save mode to: resnet50.pt")
+print("save model to: resnet50.pt")
