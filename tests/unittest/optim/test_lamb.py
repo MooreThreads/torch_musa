9b3d05964ac03a3ecd3138dc1c9149c878f95418
HanHaowen <haowen.han@mthreads.com>
2023-12-01 16:52:32 +0800

    feat: S80 ci (#595)
    * setup.py s80es and lamb multi gpu
    
    * Jenkisfile revised for s80 ci
    
    * lint
    
    * ci(s80): use docker agent instead of k8s to run s80 ci
    
    ---------
    
    Co-authored-by: Cui Yunfan <yunfan.cui@mthreads.com>

--- tests/unittest/optim/test_lamb.py
+++ tests/unittest/optim/test_lamb.py
@@ -6,23 +6,25 @@ from torch.optim import Optimizer
 from torch_musa.optim import FusedLAMB
 from torch_musa.multi_tensor_apply import multi_tensor_applier
 from torch_musa.utils import ext_loader
+import torch_musa
+
 
 
 class RefLAMB(Optimizer):
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6, weight_decay=0.01):
-        defaults = {"lr": lr, "betas": betas,
-                    "eps": eps, "weight_decay": weight_decay}
+    def __init__(
+        self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6, weight_decay=0.01
+    ):
+        defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay}
         super().__init__(params, defaults)
         if multi_tensor_applier.available:
             ext_module = ext_loader.load_ext(
-                "_ext",
-                ["multi_tensor_l2norm", "multi_tensor_lamb"]
+                "_ext", ["multi_tensor_l2norm", "multi_tensor_lamb"]
             )
             self.multi_tensor_l2norm = ext_module.multi_tensor_l2norm
             # Skip buffer
-            self._dummy_overflow_buf = torch.tensor([0],
-                                                    dtype=torch.int,
-                                                    device=self.param_groups[0]["params"][0].device)
+            self._dummy_overflow_buf = torch.tensor(
+                [0], dtype=torch.int, device=self.param_groups[0]["params"][0].device
+            )
             self.multi_tensor_lamb = ext_module.multi_tensor_lamb
         else:
             raise RuntimeError("torch_musa.optim.FusedLAMB requires musa extensions")
@@ -35,7 +37,7 @@ class RefLAMB(Optimizer):
         # create separate grad lists for fp32 and fp16 params
         g_all_32, g_all_16 = [], []
         for group in self.param_groups:
-            for p in group['params']:
+            for p in group["params"]:
                 if p.grad is None:
                     continue
                 if p.dtype == torch.float32:
@@ -43,24 +45,28 @@ class RefLAMB(Optimizer):
                 elif p.dtype == torch.float16:
                     g_all_16.append(p.grad.data)
                 else:
-                    raise RuntimeError('FusedLAMB only support fp16 and fp32.')
+                    raise RuntimeError("FusedLAMB only support fp16 and fp32.")
 
         device = self.param_groups[0]["params"][0].device
-        g_norm_32, g_norm_16 = torch.zeros(1, device=device), torch.zeros(1, device=device)
+        g_norm_32, g_norm_16 = torch.zeros(1, device=device), torch.zeros(
+            1, device=device
+        )
         # compute grad norm for two lists
         if len(g_all_32) > 0:
-            g_norm_32 = multi_tensor_applier(self.multi_tensor_l2norm,
-                                             self._dummy_overflow_buf,
-                                             [g_all_32], False)[0]
+            g_norm_32 = multi_tensor_applier(
+                self.multi_tensor_l2norm, self._dummy_overflow_buf, [g_all_32], False
+            )[0]
         if len(g_all_16) > 0:
-            g_norm_16 = multi_tensor_applier(self.multi_tensor_l2norm,
-                                             self._dummy_overflow_buf,
-                                             [g_all_16], False)[0]
+            g_norm_16 = multi_tensor_applier(
+                self.multi_tensor_l2norm, self._dummy_overflow_buf, [g_all_16], False
+            )[0]
         # blend two grad norms to get global grad norm
-        global_grad_norm = multi_tensor_applier(self.multi_tensor_l2norm,
-                                                self._dummy_overflow_buf,
-                                                [[g_norm_32, g_norm_16]],
-                                                False)[0]
+        global_grad_norm = multi_tensor_applier(
+            self.multi_tensor_l2norm,
+            self._dummy_overflow_buf,
+            [[g_norm_32, g_norm_16]],
+            False,
+        )[0]
         max_grad_norm = 1.0
         clipped_ratio = max_grad_norm / max(global_grad_norm, max_grad_norm)
 
@@ -71,25 +77,27 @@ class RefLAMB(Optimizer):
                 p.grad.data *= clipped_ratio
                 grad = p.grad.data
                 if grad.is_sparse:
-                    raise RuntimeError("FusedLAMB does not support sparse gradients, "
-                                       "please consider SparseAdam instead")
+                    raise RuntimeError(
+                        "FusedLAMB does not support sparse gradients, "
+                        "please consider SparseAdam instead"
+                    )
 
                 state = self.state[p]
                 # State initialization
                 if len(state) == 0:
                     state["step"] = 0
                     # Exponential moving average of gradient values
-                    state['m'] = torch.zeros_like(p.data)
+                    state["m"] = torch.zeros_like(p.data)
                     # Exponential moving average of gradient values
-                    state['v'] = torch.zeros_like(p.data)
+                    state["v"] = torch.zeros_like(p.data)
 
                 m_t, v_t = state["m"], state["v"]
                 beta1, beta2 = group["betas"]
 
                 state["step"] += 1
 
-                m_t.mul_(beta1).add_(grad, alpha=1-beta1)
-                v_t.mul_(beta2).addcmul_(grad, grad, value=1-beta2)
+                m_t.mul_(beta1).add_(grad, alpha=1 - beta1)
+                v_t.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
 
                 # Debiasing
                 m_t_hat = m_t / (1.0 - beta1 ** state["step"])
@@ -112,7 +120,7 @@ class RefLAMB(Optimizer):
 
                 step_size = group["lr"]
 
-                p.data.add_(update, alpha=-step_size*trust_ratio)
+                p.data.add_(update, alpha=-step_size * trust_ratio)
 
         return loss
 
@@ -157,9 +165,15 @@ class TestLamb:
         weight_decay = [0, 0.01]
 
         for wd in weight_decay:
-            lamb_option = {'lr':5e-4, 'betas':(0.9, 0.999), 'eps':1e-08, 'weight_decay':wd}
-            ref_param, tst_param, ref_optim, tst_optim = \
-                self.gen_param_optim([tensor], lamb_option)
+            lamb_option = {
+                "lr": 5e-4,
+                "betas": (0.9, 0.999),
+                "eps": 1e-08,
+                "weight_decay": wd,
+            }
+            ref_param, tst_param, ref_optim, tst_optim = self.gen_param_optim(
+                [tensor], lamb_option
+            )
 
             for _ in range(self.iters):
                 self.gen_grad(ref_param, tst_param)
@@ -183,6 +197,8 @@ class TestFusedLAMB(TestLamb):
         self.gen_single_type_test(param_type=torch.float32)
 
     def test_multi_device(self):
+        if torch_musa.device_count() < 2:
+            return
         devices = ("musa:0", "musa:1")
         for current_dev, tensor_dev in product(devices, devices):
             with torch.musa.device(current_dev):
@@ -193,12 +209,18 @@ class TestFusedLAMB(TestLamb):
         weight_decay = [0, 0.01]
 
         for wd in weight_decay:
-            lamb_option = {'lr':5e-4, 'betas':(0.9, 0.999), 'eps':1e-08, 'weight_decay':wd}
+            lamb_option = {
+                "lr": 5e-4,
+                "betas": (0.9, 0.999),
+                "eps": 1e-08,
+                "weight_decay": wd,
+            }
             tensors = []
             for size in sizes:
-                tensors.append(torch.rand(size, dtype=torch.float, device='musa'))
-            ref_param, tst_param, ref_optim, tst_optim = \
-                self.gen_param_optim(tensors, lamb_option)
+                tensors.append(torch.rand(size, dtype=torch.float, device="musa"))
+            ref_param, tst_param, ref_optim, tst_optim = self.gen_param_optim(
+                tensors, lamb_option
+            )
 
             for _ in range(self.iters):
                 self.gen_grad(ref_param, tst_param)
@@ -215,9 +237,15 @@ class TestFusedLAMB(TestLamb):
         weight_decay = [0, 0.01]
 
         for wd in weight_decay:
-            lamb_option = {"lr": 0.01, "betas": (0.6, 0.9), "eps": 3e-6, "weight_decay": wd}
-            ref_param, tst_param, ref_optim, tst_optim = \
-                self.gen_param_optim([tensor], lamb_option)
+            lamb_option = {
+                "lr": 0.01,
+                "betas": (0.6, 0.9),
+                "eps": 3e-6,
+                "weight_decay": wd,
+            }
+            ref_param, tst_param, ref_optim, tst_optim = self.gen_param_optim(
+                [tensor], lamb_option
+            )
 
             for _ in range(self.iters):
                 self.gen_grad(ref_param, tst_param)
@@ -229,7 +257,9 @@ class TestFusedLAMB(TestLamb):
                 assert max_rel_diff <= self.max_rel_diff
 
     def run(self):
-        test_funcs = [m for m in dir(self) if callable(getattr(self, m)) and m.startswith("test")]
+        test_funcs = [
+            m for m in dir(self) if callable(getattr(self, m)) and m.startswith("test")
+        ]
         for func in test_funcs:
             getattr(self, func)()
 
