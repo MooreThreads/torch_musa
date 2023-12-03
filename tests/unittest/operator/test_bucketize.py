ea32fe5869f027ad82d85bd9aa6b0abc40429b90
MoFan <fan.mo@mthreads.com>
2023-12-03 15:57:27 +0800

    feat(Op): muDNN op Bucketize ported (#596)
    * Bucketize ported
    
    * update
    
    * fix lint

--- tests/unittest/operator/test_bucketize.py
+++ tests/unittest/operator/test_bucketize.py
@@ -7,15 +7,18 @@ import torch_musa
 from torch_musa import testing
 
 input_data = testing.get_raw_data()
-# dtype of input tensor of bucketize only support Float32 in muDNN now.
-support_dtypes = [torch.float32]
+support_dtypes = [torch.float32, torch.int64, torch.int32]
+rights = [True, False]
+
 
 @testing.test_on_nonzero_card_if_multiple_musa_device(1)
 @pytest.mark.parametrize("input_data", input_data)
 @pytest.mark.parametrize("dtype", support_dtypes)
-def test_bucketize(input_data, dtype):
+@pytest.mark.parametrize("right", rights)
+def test_bucketize(input_data, dtype, right):
     input_args = {}
     input_args["input"] = input_data.to(dtype)
     input_args["boundaries"] = torch.tensor([2.0, 4.0])
+    input_args["right"] = right
     test = testing.OpTest(func=torch.bucketize, input_args=input_args)
     test.check_result()
