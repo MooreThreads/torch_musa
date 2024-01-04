d56cd32682a473d6c8d214245f2d846da43f8876
lms <maosheng.liao@mthreads.com>
2024-01-04 18:02:58 +0800

    fix IWYU.

--- torch_musa/csrc/aten/ops/Binary.cpp
+++ torch_musa/csrc/aten/ops/Binary.cpp
@@ -80,8 +80,6 @@
 #include "torch_musa/csrc/aten/utils/Utils.h"
 #include "torch_musa/csrc/utils/register_wrapper.h"
 
-#include <ATen/ops/slice.h>
-#include <limits.h>
 #include <mudnn.h>
 
 namespace at {
